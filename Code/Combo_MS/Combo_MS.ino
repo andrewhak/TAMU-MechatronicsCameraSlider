a3ca3e712b6cfa0f79ea84f11dc4de51d24e3231
Lucas1900 <40607328+Lucas1899@users.noreply.github.com>
2022-11-20 15:12:52 -0600

    Update Combo_MS.ino
    Wrote a zeroing function, and have camera go back and forth forever.
    
    To do:
    -Improve zeroing
    -Implement actual feedback for position
    -Try having it rotate as it moves

--- Code/Combo_MS/Combo_MS.ino
+++ Code/Combo_MS/Combo_MS.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>
 #include <AS5600.h>
-#include "TCA9548.h"
+#include <TCA9548.h>
 
 // Sensor Stuff
 
@@ -16,6 +16,7 @@ TCA9548 MP(0x70);
 
 int zeroPin = 18; // Limit switch's interupt pin is digital pin 18
 volatile int oTimeHit = 0; // Keeps track of when the limite was last hit
+volatile int cTimeHit = 0; // Keeps track of when the limite was last hit
 const unsigned long debounceTime = 10;
 
 int SDAPin = 20; // I2C Pins
@@ -34,19 +35,26 @@ volatile float travO = 0; // Old Value of the Traverse Encoder
 volatile float vel = 0;
 float initTOffset = 0;
 
+unsigned long oTime = 0;
+unsigned long oLoopTime = 0;
+
 // Motor Stuff
 
 #define enablePin 26                         //Define motor enable pin
-#define travDirPin 23                        //Define travel & rotation stepper motor pins
-#define travStepPin 22
-#define rotDirPin 25
-#define rotStepPin 24
+#define travDirPin 25                        //Define travel & rotation stepper motor pins
+#define travStepPin 24
+#define rotDirPin 23
+#define rotStepPin 22
 
 #define initialDur 120 
 
-float travTime = initialDur;  
+bool travstate = false;
+bool rotstate = false;
 
-int travelPulses = 100;
+//int steps1 = 16 * 50;
+//int steps2 = 16 * 25;
+//
+//float timemilsec = 10 * 1000;
 
 void setup() {
 
@@ -56,11 +64,15 @@ void setup() {
 
   // Motor Stuff
   
-  pinMode(enablePin, INPUT);                              //Open circuit enable pin, disables motors
   pinMode(travDirPin, OUTPUT);                            //Define the travel stepper motor pins
   pinMode(travStepPin, OUTPUT);
   pinMode(rotDirPin, OUTPUT);                             //Define the rotation stepper motor pins
   pinMode(rotStepPin, OUTPUT);
+  pinMode(enablePin, OUTPUT);
+
+
+  digitalWrite(travStepPin, LOW);
+  digitalWrite(rotStepPin, LOW);
 
   // Sensor Stuff
     
@@ -118,55 +130,145 @@ void setup() {
   initTOffset = tEncoder.rawAngle()* AS5600_RAW_TO_DEGREES;
   travO = initTOffset;
   tEncoder.setOffset(initTOffset);
+
+  goToZero();
 }
 
 void loop() {
-  unsigned long cTime = millis();
-  if (cTime % 50 == 0){
-    Serial.println(getTrav());
-    setZero();
+  unsigned long cLoopTime = millis();
+  
+  //if (cLoopTime - oLoopTime >= 50){
+  //    Serial.println(getTrav());
+  //    oLoopTime = cLoopTime;
+  //}
+  // Motor Code
+  
+  float currentPos = getTrav();
+  while (currentPos < 5.0){
+    currentPos = getTrav();
+    cLoopTime = millis();
+    if (cLoopTime - oLoopTime >= 50){
+        Serial.println(getTrav());
+        oLoopTime = cLoopTime;
+    }
+    Serial.println(currentPos);
+    moveMotor(0, 5, 15);
+  }
+  delay(1000);
+  while (currentPos > 0.5){
+    currentPos = getTrav();
+    cLoopTime = millis();
+    if (cLoopTime - oLoopTime >= 50){
+        Serial.println(getTrav());
+        oLoopTime = cLoopTime;
+    }
+    Serial.println(currentPos);
+    moveMotor(0, -5, 15);
+  }
+}
+
+void goToZero(){
+  
+  while (inZero == false){
+    unsigned long cTime = millis();
+    moveMotor(0, -2, 6);
+    if (cTime - oTime >= 50){
+      Serial.println(getTrav());
+      oTime = cTime;
+    }
+  }
+  Serial.println(getTrav());
+  moveMotor(0, 50, 50);
+  setZero();
+  Serial.println("Zero'd!");
+}
+
+void moveMotor(int steps1, int steps2, unsigned long timemilsec) {
+  // put your main code here, to run repeatedly:
+
+  float interval1;
+  float interval2;
+
+  unsigned long curint1time;
+  unsigned long offset1;
+
+  unsigned long curint2time;
+  unsigned long offset2;
+
+  unsigned long elapTime;
+  int curstep1 = 0;
+  int curstep2 = 0;
+  int maxsteps;
+
+  int tracker = 0;
+  bool step1state = false;
+  bool step2state = false;
+
+
+  if (steps1 < 0) {
+    digitalWrite(rotDirPin, HIGH);
+    steps1 = abs(steps1);
+  } else {
+    digitalWrite(rotDirPin, LOW);
+  }
+  if (steps2 < 0) {
+    digitalWrite(travDirPin, HIGH);
+    steps2 = abs(steps2);
+  } else {
+    digitalWrite(travDirPin, LOW);
   }
 
-  // Motor Code
+  if (steps1 > steps2) {
+    maxsteps = steps1;
+    tracker = curstep1;
 
-  // Need to figure out how to merge, maybe make sensor code a func that outputs the current position
-  // and have the motor code then use that?
-  
-  //pinMode(enablePin, OUTPUT);
+  } else {
+    maxsteps = steps2;
+    tracker = curstep2;
+  }
 
-  //float interval = calcInterval (travelPulses);         //Calculate the pulse interval required to move the required distance in the required time
+  interval1 = timemilsec / steps1;
+  interval2 = timemilsec / steps2;
 
-  // put your main code here, to run repeatedly:
-  //for (int i = 1; i <= travelPulses; i++)               //Pulse the motor to move the required distance in the required time
-  //{
-  //  digitalWrite(travStepPin, HIGH);
-  //  delayMicroseconds(interval / 2);
-  //  digitalWrite(travStepPin, LOW);
-  //  delayMicroseconds(interval / 2);
-  //}
+  while (!step1state or !step2state) {
+    elapTime = millis();
+    curint1time = elapTime - offset1;
+    curint2time = elapTime - offset2;
+    if (curstep1 < steps1) {
+      if (curint1time > (interval1 / 2) and  rotstate == false) {
+        digitalWrite(rotStepPin, HIGH);
+        offset1 = elapTime;
+        curstep1++;
+        rotstate = true;
+      }
+      curint1time = elapTime - offset1;
+      if (curint1time > (interval1 / 2) and rotstate == true ) {
+        digitalWrite(rotStepPin, LOW);
+        offset1 = elapTime;
+        rotstate = false;
+      }
+    } else {
+      step1state = true;
+    }
 
-  //delay(1000);
+    if (curstep2 < steps2) {
+      if (curint2time > (interval2 / 2) and  travstate == false) {
+        digitalWrite(travStepPin, HIGH);
+        offset2 = elapTime;
+        curstep2++;
+        travstate = true;
 
-  //for (int i = 1; i <= travelPulses; i++)               //Pulse the motor to move the required distance in the required time
-  //{
-  //  digitalWrite(rotStepPin, HIGH);
-  //  delayMicroseconds(interval / 2);
-  //  digitalWrite(rotStepPin, LOW);
-  //  delayMicroseconds(interval / 2);
-  //}
-
-}
-
-float calcInterval (int numPulses)                                 //Calculate the interval required between pulses to achieve duration
-{
-  float inter = travTime * 1000000 / numPulses;
-  return inter;
-}
-
-float calcRotInterval (int numPulses)                              //Calculate the interval required between pulses to achieve duration
-{
-  float inter = travTime * 1000 / numPulses;
-  return inter;
+      }
+      curint2time = elapTime - offset2;
+      if (curint2time > (interval2 / 2) and travstate == true ) {
+        digitalWrite(travStepPin, LOW);
+        offset2 = elapTime;
+        travstate = false;
+      }
+    } else {
+      step2state = true;
+    }
+  }
 }
 
 float getPan(){
@@ -210,13 +312,13 @@ float getTrav(){
 }
 
 void setZero(){
-  if (inZero == true) {
-    float zero = tEncoder.rawAngle() * AS5600_RAW_TO_DEGREES;
-    tEncoder.setOffset(zero);
-    travRots = 0;
-    travO = 0;
-  }
-  inZero = false;
+  MP.selectChannel(0);
+  float zero = getTrav();
+  tEncoder.setOffset(zero);
+  Serial.println(zero);
+  Serial.println(tEncoder.getOffset());
+  travRots = 0;
+  travO = 0;
 }
 
 void hitZero() { // This needs work
