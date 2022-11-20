e80fda938405ad78973cbeaff78ebc3fce3033b3
andrewhak <47075987+andrewhak@users.noreply.github.com>
2022-11-20 11:53:31 -0600

    Update 2Motor_Operation.ino
    added second motor control

--- Code/2Motor_Operation/2Motor_Operation.ino
+++ Code/2Motor_Operation/2Motor_Operation.ino
@@ -7,10 +7,10 @@
 bool travstate = false;
 bool rotstate = false;
 
-int steps1 = 16 * 200;
-int steps2 = 16 * 100;
+int steps1 = 16 * 50;
+int steps2 = 16 * 25;
 
-float timemilsec = 1 * 1000;
+float timemilsec = 10 * 1000;
 
 float interval1 = 0;
 float interval2 = 0;
@@ -18,8 +18,13 @@ float interval2 = 0;
 unsigned long curint1time;
 unsigned long offset1;
 
+unsigned long curint2time;
+unsigned long offset2;
+
+
 unsigned long elapTime;
 int curstep1 = 0;
+int curstep2 = 0;
 int maxsteps = 0;
 
 void setup() {
@@ -48,22 +53,36 @@ void loop() {
   if (curstep1 < maxsteps) {
     elapTime = millis();
     curint1time = elapTime - offset1;
+    curint2time = elapTime - offset2;    
     Serial.println(curstep1);
     if (curstep1 < steps1) {
-      if (curint1time > interval1 / 2 and  travstate == false) {
+      if (curint1time > interval1 / 2 and  rotstate == false) {
         digitalWrite(rotStepPin, HIGH);
         offset1 = elapTime;
         curstep1++;
+        rotstate = true;
+
+      }
+      if (curint1time > interval1 / 2 and rotstate == true ) {
+        digitalWrite(rotStepPin, LOW);
+        offset1 = elapTime;
+        rotstate = false;
+      }
+    }
+    if (curstep2 < steps2) {
+      if (curint1time > interval1 / 2 and  travstate == false) {
+        digitalWrite(travStepPin, HIGH);
+        offset2 = elapTime;
+        curstep2++;
         travstate = true;
 
       }
-      if (curint1time > interval1 / 2 and travstate == true ) {
-        digitalWrite(rotStepPin, LOW);
-        offset1 = elapTime;
+      if (curint2time > interval2 / 2 and travstate == true ) {
+        digitalWrite(travStepPin, LOW);
+        offset2 = elapTime;
         travstate = false;
       }
     }
-    
   }
 
 }
