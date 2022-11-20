e7d9cc52468ca85491320043a0c1e729dc6d87ef
andrewhak <47075987+andrewhak@users.noreply.github.com>
2022-11-20 13:50:50 -0600

    Update 2Motor_Operation.ino

--- Code/2Motor_Operation/2Motor_Operation.ino
+++ Code/2Motor_Operation/2Motor_Operation.ino
@@ -7,25 +7,13 @@
 bool travstate = false;
 bool rotstate = false;
 
-int steps1 = 16 * 50;
-int steps2 = 16 * 25;
-
-float timemilsec = 10 * 1000;
-
-float interval1 = 0;
-float interval2 = 0;
-
-unsigned long curint1time;
-unsigned long offset1;
-
-unsigned long curint2time;
-unsigned long offset2;
+//int steps1 = 16 * 50;
+//int steps2 = 16 * 25;
+//
+//float timemilsec = 10 * 1000;
 
 
-unsigned long elapTime;
-int curstep1 = 0;
-int curstep2 = 0;
-int maxsteps = 0;
+
 
 void setup() {
   //Open circuit enable pin, disables motors
@@ -36,52 +24,103 @@ void setup() {
   pinMode(rotStepPin, OUTPUT);
   pinMode(enablePin, OUTPUT);
 
-  interval1 = timemilsec / steps1;
-  interval2 = timemilsec / steps2;
+
   digitalWrite(travStepPin, LOW);
   digitalWrite(rotStepPin, LOW);
+
+}
+void loop() {
+  moveMotor(200, 50, 1000);
+  delay(1000);
+
+  moveMotor(-200, -50, 1000);
+
+}
+
+void moveMotor(int steps1, int steps2, unsigned long timemilsec) {
+  // put your main code here, to run repeatedly:
+
+  float interval1;
+  float interval2;
+
+  unsigned long curint1time;
+  unsigned long offset1;
+
+  unsigned long curint2time;
+  unsigned long offset2;
+
+  unsigned long elapTime;
+  int curstep1 = 0;
+  int curstep2 = 0;
+  int maxsteps;
+
+  int tracker = 0;
+  bool step1state = false;
+  bool step2state = false;
+
+
+  if (steps1 < 0) {
+    digitalWrite(rotDirPin, HIGH);
+    steps1 = abs(steps1);
+  } else {
+    digitalWrite(rotDirPin, LOW);
+  }
+  if (steps2 < 0) {
+    digitalWrite(travDirPin, HIGH);
+    steps2 = abs(steps2);
+  } else {
+    digitalWrite(travDirPin, LOW);
+  }
+
   if (steps1 > steps2) {
     maxsteps = steps1;
+    tracker = curstep1;
 
   } else {
     maxsteps = steps2;
+    tracker = curstep2;
   }
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if (curstep1 < maxsteps) {
+  interval1 = timemilsec / steps1;
+  interval2 = timemilsec / steps2;
+
+  while (!step1state or !step2state) {
     elapTime = millis();
     curint1time = elapTime - offset1;
-    curint2time = elapTime - offset2;    
-    Serial.println(curstep1);
+    curint2time = elapTime - offset2;
     if (curstep1 < steps1) {
-      if (curint1time > interval1 / 2 and  rotstate == false) {
+      if (curint1time > (interval1 / 2) and  rotstate == false) {
         digitalWrite(rotStepPin, HIGH);
         offset1 = elapTime;
         curstep1++;
         rotstate = true;
-
       }
-      if (curint1time > interval1 / 2 and rotstate == true ) {
+      curint1time = elapTime - offset1;
+      if (curint1time > (interval1 / 2) and rotstate == true ) {
         digitalWrite(rotStepPin, LOW);
         offset1 = elapTime;
         rotstate = false;
       }
+    } else {
+      step1state = true;
     }
+
     if (curstep2 < steps2) {
-      if (curint1time > interval1 / 2 and  travstate == false) {
+      if (curint2time > (interval2 / 2) and  travstate == false) {
         digitalWrite(travStepPin, HIGH);
         offset2 = elapTime;
         curstep2++;
         travstate = true;
 
       }
-      if (curint2time > interval2 / 2 and travstate == true ) {
+      curint2time = elapTime - offset2;
+      if (curint2time > (interval2 / 2) and travstate == true ) {
         digitalWrite(travStepPin, LOW);
         offset2 = elapTime;
         travstate = false;
       }
+    } else {
+      step2state = true;
     }
   }
 
